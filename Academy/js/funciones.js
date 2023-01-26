f07bdba329818b9962b60f92b06300f68e97bdf3
Juan Manuel Varela <jmvarela-@hotmail.com>
2023-01-26 09:30:30 -0300

    Add files via upload

---
+++ Academy/js/funciones.js
@@ -0,0 +1,39 @@
+function mouseOver() {
+  document.getElementById("titulo").style.color = "black";
+}
+function mouseOut() {
+  document.getElementById("titulo").style.color = "white";
+}
+function myNombre() {
+  document.getElementById("nombre").innerHTML = "HAROLD DIXON";
+}
+function myDNI() {
+  document.getElementById("dni").innerHTML = "0909090909";
+}
+function myNacimiento() {
+  document.getElementById("nacimiento").innerHTML = "07/02/1974";
+}
+function myNacionalidad() {
+  document.getElementById("nacionalidad").innerHTML = "ARGENTINA";
+}
+function myProvincia() {
+  document.getElementById("provincia").innerHTML = "BUENOS AIRES";
+}
+function myLocalidad() {
+  document.getElementById("localidad").innerHTML = "LA PLATA";
+}
+function myDireccion() {
+  document.getElementById("direccion").innerHTML = "Poplar DR";
+}
+function myCodigoPostal() {
+  document.getElementById("codigopostal").innerHTML = "2753";
+}
+function myEstadoCivil() {
+  document.getElementById("estadocivil").innerHTML = "CASADO";
+}
+function myEmail() {
+  document.getElementById("email").innerHTML = "harold.dixon@example.com";
+}
+function myTelefono() {
+  document.getElementById("telefono").innerHTML = "(610) 302-7810";
+}
